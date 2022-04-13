669a9bffd73935923011b375055628e7ae2a10b7
AmayaGM <103547060+AmayaGM@users.noreply.github.com>
2022-04-13 18:27:12 -0500

    AG2

---
+++ app/src/main/java/com/example/duxeles/Mod_Bebida.java
@@ -0,0 +1,14 @@
+package com.example.duxeles;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Mod_Bebida extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
